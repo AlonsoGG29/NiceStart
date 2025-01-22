7ddb469f027e3edf9efd9baefff4ca519b55578e
AlonsoGG29 <gomez.alonso07@gmail.com>
2025-01-22 12:27:59 +0100

    Poner logos en el boton_sheet_layout

--- app/src/main/java/com/agomez/nicestart/mainBab.java
+++ app/src/main/java/com/agomez/nicestart/mainBab.java
@@ -1,8 +1,10 @@
 package com.agomez.nicestart;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class mainBab extends AppCompatActivity {
@@ -56,8 +59,6 @@ public class mainBab extends AppCompatActivity {
                 Toast.makeText(mainBab.this, "Menu clicked", Toast.LENGTH_SHORT).show();
 //                sheetBehavior = BottomSheetBehavior.from(sheet);
             }
-
-
         });
 
 
@@ -82,5 +83,49 @@ public class mainBab extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // Método para mostrar el BottomSheetDialog
+        private void showBottomSheetDialog() {
+            // Inflar el diseño del BottomSheet
+            View view = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_layout, null);
+
+            // Crear el BottomSheetDialog
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+            bottomSheetDialog.setContentView(view);
+            bottomSheetDialog.show();
+
+            // Configurar listeners para las opciones del BottomSheet
+            TextView option1 = view.findViewById(R.id.option1);
+            TextView option2 = view.findViewById(R.id.option2);
+            TextView option3 = view.findViewById(R.id.option3);
+
+            option1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Acción para Opción 1
+                    Toast.makeText(mainBab.this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                    bottomSheetDialog.dismiss();
+                }
+            });
+
+            option2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Acción para Opción 2
+                    Toast.makeText(mainBab.this, "About clicked", Toast.LENGTH_SHORT).show();
+                    bottomSheetDialog.dismiss();
+                }
+            });
+
+            option3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Acción para Opción 3
+                    Toast.makeText(mainBab.this, "Logout clicked", Toast.LENGTH_SHORT).show();
+                    bottomSheetDialog.dismiss();
+                }
+            });
+        }
     }
 }
